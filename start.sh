321ad77440aefa5ae211de9ecf9194d52c61045c
Reinier Rodriguez Gonzalez <reinier.rodriguez@undostres.com.mx>
2021-03-07 20:06:11 -0600

    Fix start.sh db volume

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ docker rm mongo-ebc-container
 docker run --name mongo-ebc-container \
   -e MONGO_INITDB_DATABASE=everlywell_backend_challenge_development \
   -p 27017:27017  \
-  -v mongodb_ebc_data_container:/data/ebc_db  \
+  -v mongodb_ebc_data_container:/data/db  \
   -d mongo
 
 bundle exec rails s -p 3000
