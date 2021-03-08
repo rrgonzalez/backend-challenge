68f3b13939f145bb01314ba02580f1ad22e69fd4
Reinier Rodriguez Gonzalez <reinier.rodriguez@undostres.com.mx>
2021-03-07 19:33:44 -0600

    Setup headings crawling on Member creation

--- app/controllers/member_controller.rb
+++ app/controllers/member_controller.rb
@@ -3,10 +3,17 @@ class MemberController < ApplicationController
   def create
     @member = Member.new(
       name: params[:name],
-      website_url: params[:website_url]
+      website_url: params[:website_url],
+      friends: []
     )
 
-    @member.website_short_url = ShortUrlController::short_url(long_url: @member.website_url)
+    # Shorten URL
+    @member.website_short_url = ShortUrlController.short_url(long_url: @member.website_url)
+
+    # Crawl h1-h3 headings from member website
+    heading_strs = CrawlerController.crawl_headings(@member.website_url)
+
+    @member.headings = build_headings(heading_strs, @member)
 
     res = @member.save!
 
@@ -25,6 +32,33 @@ class MemberController < ApplicationController
   end
 
   def add_friend
+  end
+
+  private
+
+  def build_headings(heading_strs, member)
+    Rails.logger.info heading_strs
+
+    headings = []
+
+    # Create the Heading document for each found heading
+    heading_strs.each do |heading_str|
+      terms = heading_str.split
+      # Rails.logger.info terms
+      hashed_terms = Set.new
+
+      # By converting the string to symbol it is being hashed
+      # Symbols can be compared faster than strings, this is useful for expert search
+      terms.each do |term|
+        term = term.gsub(/[^0-9a-zA-Z]/i, '')
+        hashed_terms.add(term.to_sym)
+      end
+
+      heading = Heading.create!(member: @member, pretty_print: heading_str, hashed_terms: hashed_terms)
+      headings << heading
+    end
+
+    headings
   end
 
 end
